94ea592e4ecb3a6f2cc09ac7583378f37e3178fd
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-04-27 16:09:19 +0100

    create react remove boilerplate add flask

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+      <h1>Daddy Nappy Change</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+
+
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
