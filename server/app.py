f16a942f67cdd2b8a767a795524f4529fd475e18
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-05-04 21:12:59 +0100

    backend for viewing 1 venue done

--- server/app.py
+++ server/app.py
@@ -5,33 +5,9 @@ import db
 app = Flask(__name__)
 CORS(app)
 
-# venuesData = [
-#         {
-#             "name": "National Museum of Scotland",
-#             "lat": 55.9469809,
-#             "lng": -3.1905524,
-#             "placeId" : "ChIJIeS60YTHh0gRw7W-8gMnzIg"
-#         },
-#         {
-#             "name": "National Gallery of Scotland",
-#             "lat": 55.950902,
-#             "lng": -3.1956862,
-#             "placeId": "ChIJ68bm6kjGh0gRkyF0XlT5Rww"
-#         }
-#     ]
-
 @app.route('/')
 def index():
     return '<h1>Daddy Nappy Change<h2><p>Welcome to the back-end...</p><p><a href="https://daddynappychange.herokuapp.com/api/venues">Link to venues json</a></p>'
-
-# @app.route('/api/venues', methods=['POST', 'GET'])
-# def venues():
-#     if request.method == 'POST':
-#         newVenue = request.get_json()
-#         venuesData.append(newVenue)
-#         db.add_venue(newVenue)
-#         return jsonify(venuesData)
-#     return jsonify(venuesData)
 
 @app.route('/api/venues', methods=['POST', 'GET'])
 def db_venues():
@@ -44,11 +20,12 @@ def db_venues():
 @app.route('/api/venues/<id>')
 def getVenue(id):
     print(id)
-    for venue in venuesData:
-        print(venue['placeId'])
-        if venue['placeId'] == id:
-            return jsonify(venue)
-    return {}
+    return jsonify(db.get_venue_by_placeid(id))
+    # for venue in venuesData:
+    #     print(venue['placeId'])
+    #     if venue['placeId'] == id:
+    #         return jsonify(venue)
+    # return {}
 
 if __name__ == '__main__':
     app.run()
